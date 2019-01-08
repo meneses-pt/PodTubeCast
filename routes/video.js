051ce54a4dd6d002dfacd3768801fbdaee49b88e
André Meneses <andremeneses@gmail.com>
2019-01-08 12:17:52 +0000

    Changed youtube downloader

--- routes/video.js
+++ routes/video.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const youtubedl = require('youtube-dl');
+//const youtubedl = require('youtube-dl');
+const ytdl = require('ytdl-core');
 const router = express.Router();
+
+
 
 router.get("/:videoId", function(req, res, next) {
 
-    var video = youtubedl('http://www.youtube.com/watch?v=' + req.params.videoId
+    //var video = youtubedl('http://www.youtube.com/watch?v=' + req.params.videoId
     //,
-        // Optional arguments passed to youtube-dl.
-        //['-f bestaudio[ext=m4a]'],
-//        // Additional options can be given for calling `child_process.execFile()`.
-//        { cwd: __dirname }
-    );
+    //          //Optional arguments passed to youtube-dl.
+    //          ['-f bestaudio[ext=m4a]'],
+    //          // Additional options can be given for calling `child_process.execFile()`.
+    //          { cwd: __dirname }
+    //);
+    //video.pipe(res);
 
-    video.pipe(res);
+    ytdl('http://www.youtube.com/watch?v=' + req.params.videoId).pipe(res);
 });
 
 module.exports = router;
