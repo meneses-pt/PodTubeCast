03e6cf6f9d372c21972a967321a03be344a523a0
André Meneses <andremeneses@gmail.com>
2019-01-16 18:13:32 +0000

    Custom 404 message

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use("/video", videoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404, {expose: true}));
+	next(createError(404, '404! Page not found...'));
 });
 
 // error handler
