c57630bb4eb1098de1d070b3f0187d3e3213e510
André Meneses <andremeneses@gmail.com>
2019-01-17 18:01:50 +0000

    fix in redirects

--- routes/playlist.js
+++ routes/playlist.js
@@ -7,29 +7,42 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/:playlistId", function(req, res, next) {
-	var baseAddress = req.protocol + "://" + req.hostname;
+	processPlaylistRequest(req, res);
+});
 
-	var parameter;
-	
-	if(isURL(req.params.playlistId)) {
-		parameter = getYotubePlaylistId(req.params.playlistId);
+router.get("/", function(req, res, next) {
+	processPlaylistRequest(req, res);
+});
+
+function processPlaylistRequest(req, res) {
+
+	var playlistId;
+	if(req.params.playlistId != null && req.params.playlistId != "") {
+		playlistId = req.params.playlistId;
 	} else {
-		parameter = req.params.playlistId;
+		playlistId = req.query.playlistId;
 	}
 
-	ytpl(parameter, function(err, playlist) {
-		if (err) throw err;
+	var parameter;
+	if (isURL(playlistId)) {
+		parameter = getYotubePlaylistId(playlistId);
+	}
+	else {
+		parameter = playlistId;
+	}
 
+	var baseAddress = req.protocol + "://" + req.hostname;
+	ytpl(parameter, function (err, playlist) {
+		if (err)
+			throw err;
 		console.log(playlist);
-
 		/* lets create an rss feed */
 		const feed = new podcast({
 			title: playlist.title,
 			description: "[PodTubeCast] " + playlist.title,
 			feed_url: baseAddress + "/playlist/" + parameter,
 			site_url: baseAddress,
-			image_url:
-				"https://dummyimage.com/400x400/f2f7f8/40424a.jpg&text=[PodTubeCast]+" +
+			image_url: "https://dummyimage.com/400x400/f2f7f8/40424a.jpg&text=[PodTubeCast]+" +
 				encodeURI(playlist.title),
 			author: playlist.author.name,
 			//managingEditor: 'Dylan Greene',
@@ -50,45 +63,35 @@ router.get("/:playlistId", function(req, res, next) {
 				//  "text": "Television"
 				//}]
 			},
-			itunesImage:
-				"https://dummyimage.com/400x400/f2f7f8/40424a.jpg&text=[PodTubeCast]+" +
+			itunesImage: "https://dummyimage.com/400x400/f2f7f8/40424a.jpg&text=[PodTubeCast]+" +
 				encodeURI(playlist.title)
 		});
-
 		var promises = [];
-
-		playlist.items.forEach(function(element) {
-			promises.push(
-				ytdl.getBasicInfo(
-					"http://www.youtube.com/watch?v=" + element.id
-				)
-			);
+		playlist.items.forEach(function (element) {
+			promises.push(ytdl.getBasicInfo("http://www.youtube.com/watch?v=" + element.id));
 		});
-
 		Promise.all(promises).then(results => {
 			results.forEach(element => {
 				console.log(element);
-
 				item = playlist.items.find(i => i.id == element.video_id);
 				var format = ytdl.chooseFormat(element.formats, {
 					quality: "highest"
 				});
-
 				feed.addItem({
 					title: element.title,
 					description: element.description,
-					url: baseAddress + "/video/" + element.video_id, // link to the item
-					guid: element.video_id, // optional - defaults to url
+					url: baseAddress + "/video/" + element.video_id,
+					guid: element.video_id,
 					//categories: ["Category 1", "Category 2", "Category 3", "Category 4"], // optional - array of item categories
-					author: element.author.name, // optional - defaults to feed author property
-					date: element.published, // any format that js Date can parse.
+					author: element.author.name,
+					date: element.published,
 					//lat: 33.417974, //optional latitude field for GeoRSS
 					//long: -111.933231, //optional longitude field for GeoRSS
 					enclosure: {
 						url: baseAddress + "/video/" + element.video_id,
 						type: format.type /*, file:'path-to-file'*/,
 						size: format.size || 0
-					}, // optional enclosure TODO: size
+					},
 					itunesAuthor: element.author.name,
 					itunesExplicit: false,
 					//itunesSubtitle: "iTunes Subtitle " + i,
@@ -98,15 +101,13 @@ router.get("/:playlistId", function(req, res, next) {
 					itunesImage: item.thumbnail
 				});
 			});
-
 			const xml = feed.buildXml();
-
 			res.set("Content-Type", "text/xml");
 			res.send(xml);
 			console.log("Send XML");
 		});
 	});
-});
+}
 
 function isURL(str) {
 	return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(str); 
--- routes/video.js
+++ routes/video.js
@@ -3,17 +3,33 @@ const ytdl = require("ytdl-core");
 const router = express.Router();
 
 router.get("/:videoId", function(req, res, next) {
-	var parameter;
+	processVideoRequest(req, res);
+});
 
-	if (isURL(req.params.videoId)) {
-		parameter = ytdl.getURLVideoID(req.params.videoId);
+router.get("/", function(req, res, next) {
+	processVideoRequest(req, res);
+});
+
+function processVideoRequest(req, res) {
+
+	var videoId;
+	if(req.params.videoId != null && req.params.videoId != "") {
+		videoId = req.params.videoId;
 	} else {
-		parameter = req.params.videoId;
+		videoId = req.query.videoId;
+	}
+
+	var parameter;
+	if (isURL(videoId)) {
+		parameter = ytdl.getURLVideoID(videoId);
+	}
+	else {
+		parameter = videoId;
 	}
 
 	res.setHeader("Content-Type", "video/mp4");
 	ytdl(parameter, { quality: "highest" }).pipe(res);
-});
+}
 
 function isURL(str) {
 	return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(str); 
